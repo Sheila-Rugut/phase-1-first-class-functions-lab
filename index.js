4399c12f1ec7b9f58c978203cf19755db72c10ad
Sheila <rugut.cheruto@student.moringaschool.com>
2022-08-20 19:05:09 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayOfDrivers) {
+    return (arrayOfDrivers.slice(0,2));
+  }
+const returnLastTwoDrivers= function(arrayOfDrivers){
+    return (arrayOfDrivers.slice(-2));
+}
+const selectingDrivers=[ returnFirstTwoDrivers, returnLastTwoDrivers ];
+
+const createFareMultiplier= function(fareMultiplier) {
+    return function(value) {
+      return fareMultiplier * value;
+    }
+  }
+const fareDoubler= createFareMultiplier(2);
+const fareTripler= createFareMultiplier(3);
+const selectDifferentDrivers=function(drivers,driversToReturn){
+    return driversToReturn(drivers);
+}
